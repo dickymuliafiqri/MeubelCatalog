f6735ffdca2e4e7a4ad63d02a63e10bf19b77acf
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-08-10 14:41:40 +0700

    Deploy to Nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxt/scripts", "@nuxt/ui", "@nuxt/content"],
+  modules: [
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+    "@nuxt/content",
+    "@nuxthub/core",
+  ],
   css: ["~/assets/css/main.css"],
   ui: {
     colorMode: false,
